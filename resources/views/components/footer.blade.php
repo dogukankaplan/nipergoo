02e98081567743985d9f6aed334200d7cbc695e2
Doğukan Kaplan <160619803+dogukankaplan@users.noreply.github.com>
2026-01-19 15:48:25 +0300

    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -187,7 +187,7 @@
                     <span class="hidden sm:block">•</span>
                     <div class="flex items-center gap-1 text-slate-400">
                         <span>Made with</span>
-                        <svg class="w-4 h-4 text-red-500 animate-pulse" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"/></svg>
+                        <a href="#" class="font-semibold text-blue-400 hover:text-blue-300 transition-colors">Dose Software</a>
                         <span>in İzmir</span>
                     </div>
                 </div>
